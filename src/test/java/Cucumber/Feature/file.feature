ad2af7e84bfebed00d3bde059715a094b22c3cb0
Tript-Kaur <98944316+Tript-Kaur@users.noreply.github.com>
2022-04-04 00:45:50 +0530

    First Commit

---
+++ src/test/java/Cucumber/Feature/file.feature
@@ -0,0 +1,6 @@
+Feature: practice
+  Scenario: checking
+    Given new profile
+    When same Profile
+    Then Difference
+
---
+++ src/test/java/Cucumber/Steps/fileSteps.java
@@ -0,0 +1,20 @@
+package Cucumber.Steps;
+//import io.cucumber.java.en.*;
+import cucumber.api.java.en.*;
+
+public class fileSteps {
+   @Given("^new profile$")
+    public void newProfile() {
+        System.out.println("1");
+    }
+
+    @When("^same Profile$")
+    public void sameProfile() {
+        System.out.println("2");
+    }
+
+    @Then("^Difference$")
+    public void difference() {
+        System.out.println("3");
+    }
+}
