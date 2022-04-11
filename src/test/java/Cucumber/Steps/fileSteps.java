e602fc74a51f56e1f0d32d86b84ec8b934c99374
Tript Kaur <triptkaur@gmail.com>
2022-04-11 12:25:50 +0530

    Log changes

--- src/test/java/Cucumber/Steps/fileSteps.java
+++ src/test/java/Cucumber/Steps/fileSteps.java
@@ -2,20 +2,24 @@ package Cucumber.Steps;
 //import io.cucumber.java.en.*;
 import cucumber.api.java.en.*;
 
+import java.util.logging.Logger;
+
 public class fileSteps {
+    Logger log= Logger.getLogger("Creating Logs");
    @Given("^new profile$")
     public void newProfile() {
-        System.out.println("1");
+        System.out.println("1"); log.info("inside given step");
     }
 
     @When("^same Profile$")
     public void sameProfile() {
         System.out.println("2");
-        System.out.println("Making changes");
+        System.out.println("Making changes twice"); log.info("inside when step");
     }
 
     @Then("^Difference$")
     public void difference() {
         System.out.println("3");
+        log.info("inside Then step");
     }
 }
